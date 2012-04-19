36e139e1577c01fae7a5f4d43ee684cd111f8742
Johannes Laier <Johannes.laier@web.de>
2012-04-19 11:43:26 +0200

    First Example added. This example demonstrate the mysql::transaction method

---
+++ example/example1.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../mysql.class.php';
+$_mysql = new mysql;
+
+$_mysql->connect_db();
+$_mysql->query("INSERT INTO konto (1500, 636.0, 1045);");
+$_mysql->query("INSERT INTO konto (1510, 4984.30, 1052);");
+
+function func($sql) {
+	while ($row = mysql_metch_row($sql)) {
+		if ($row['id'] < 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+$funcObject = func($sql);
+
+$_mysql->transaction('
+	UPDATE SET value=value+34579.84 WHERE userid = 1045;
+	UPDATE SET value=value-34579.84 WHERE userid = 1052;
+	SELECT id, value, userid FROM konto;
+', $funcObject);
+?>
